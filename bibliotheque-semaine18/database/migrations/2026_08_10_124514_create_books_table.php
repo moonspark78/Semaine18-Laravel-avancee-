e46c4d6c4db16d48c7a41c8f3c288b8b010103f6
Omar Jabour <jabour.omar@hotmail.com>
2026-08-10 14:49:13 +0200

    :bento: tp1: Book database  migrate  :art:

--- bibliotheque-semaine18/database/migrations/2026_08_10_124514_create_books_table.php
+++ bibliotheque-semaine18/database/migrations/2026_08_10_124514_create_books_table.php
@@ -6,20 +6,16 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 255);
+            $table->foreignId('author_id')->constrained('authors')->onDelete('cascade');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('books');
