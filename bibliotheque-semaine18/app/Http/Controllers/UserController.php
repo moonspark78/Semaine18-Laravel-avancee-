e3d51b2b639cdc2de06075a9d674966c36f15e1c
Omar Jabour <jabour.omar@hotmail.com>
2026-08-10 14:17:13 +0200

    :bento: userController  :art:

--- bibliotheque-semaine18/app/Http/Controllers/UserController.php
+++ bibliotheque-semaine18/app/Http/Controllers/UserController.php
@@ -2,9 +2,79 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // $users = User::all();
+        $users = User::with('role')->get();
+
+        $user = User::first();
+        $user->name = "Boby";
+        // $user->save();
+        // dump($user);
+        dump($user->isDirty());
+        $user->save();
+
+        dump($user->wasChanged());
+        dd($user->isClean());
+        // $user = User::find(1);
+        // $users->load('role');
+        // dd($user->role);
+        return view('users.index', compact('users'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
 }
