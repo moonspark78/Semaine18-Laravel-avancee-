4f01fa18efeeb39fa7401e0fe4ca92733ef3887f
Omar Jabour <jabour.omar@hotmail.com>
2026-08-10 14:17:44 +0200

    :bento: user model  :art:

--- bibliotheque-semaine18/app/Models/Role.php
+++ bibliotheque-semaine18/app/Models/Role.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,4 +10,21 @@ class Role extends Model
 {
     /** @use HasFactory<\Database\Factories\RoleFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'name'
+    ];
+
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
+
+    protected function name(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => strtolower($value),
+            set: fn (string $value) => ucfirst(strtolower($value))
+        );
+    }
 }
