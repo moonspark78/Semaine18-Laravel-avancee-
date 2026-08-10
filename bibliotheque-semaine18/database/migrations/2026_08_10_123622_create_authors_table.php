6c15f9011f00ca91e2efd6368fe7c72fa2e85b59
Omar Jabour <jabour.omar@hotmail.com>
2026-08-10 14:39:24 +0200

    :bento: tp1 author -mfs createAuthor  :art:

--- bibliotheque-semaine18/database/migrations/2026_08_10_123622_create_authors_table.php
+++ bibliotheque-semaine18/database/migrations/2026_08_10_123622_create_authors_table.php
@@ -6,20 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('authors', function (Blueprint $table) {
             $table->id();
+            $table->string('last_name');
+            $table->string('first_name');
+            $table->string('email')->unique();
+            $table->string('phone')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('authors');
