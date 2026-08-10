2b1baab140693575b93ca152f9f3abcf514dd923
Omar Jabour <jabour.omar@hotmail.com>
2026-08-10 15:27:09 +0200

    :bento:tp1:  Model Book speeder  :art:

--- bibliotheque-semaine18/database/factories/AuthorFactory.php
+++ bibliotheque-semaine18/database/factories/AuthorFactory.php
@@ -2,23 +2,17 @@
 
 namespace Database\Factories;
 
-use App\Models\Author;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends Factory<Author>
- */
 class AuthorFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
-            //
+            'last_name' => fake()->lastName(),
+            'first_name' => fake()->firstName(),
+            'email' => fake()->unique()->safeEmail(),
+            'phone' => fake()->phoneNumber(),
         ];
     }
 }
--- bibliotheque-semaine18/database/seeders/DatabaseSeeder.php
+++ bibliotheque-semaine18/database/seeders/DatabaseSeeder.php
@@ -2,24 +2,20 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Author;
+use App\Models\Book;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $authors = Author::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        foreach ($authors as $author) {
+            Book::factory(3)->create([
+                'author_id' => $author->id,
+            ]);
+        }
     }
 }
