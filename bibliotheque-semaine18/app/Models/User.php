09c263d2636aca1c081e5a3ee5a77720cd8788d9
Omar Jabour <jabour.omar@hotmail.com>
2026-08-12 16:13:08 +0200

    :bento:TP3: roleSeeders  :art:

--- bibliotheque-semaine18/app/Models/User.php
+++ bibliotheque-semaine18/app/Models/User.php
@@ -8,12 +8,13 @@ use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasRoles;
 
     protected function casts(): array
     {
@@ -21,16 +22,6 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
-    }
-
-    public function role()
-    {
-        return $this->belongsTo(Role::class);
-    }
-
-    public function isStaffOrAdmin(): bool
-    {
-        return $this->role && in_array($this->role->name, ['admin', 'staff'], true);
     }
 
     public function loans()
--- bibliotheque-semaine18/database/seeders/DatabaseSeeder.php
+++ bibliotheque-semaine18/database/seeders/DatabaseSeeder.php
@@ -11,35 +11,42 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
+        $this->call([
+            RoleSeeder::class,
+        ]);
+
         $admin = User::factory()->create([
             'name' => 'Admin',
             'email' => 'admin@example.com',
         ]);
+
+        $admin->assignRole('admin');
 
         $staff = User::factory()->create([
             'name' => 'Staff',
             'email' => 'staff@example.com',
         ]);
 
+        $staff->assignRole('staff');
+
+        $user = User::factory()->create([
+            'name' => 'User',
+            'email' => 'user@example.com',
+        ]);
+
+        $user->assignRole('user');
+
         $authors = Author::factory(10)->create();
 
         foreach ($authors as $author) {
             Book::factory(3)->create([
                 'author_id' => $author->id,
+                'created_by' => $staff->id,
             ]);
         }
 
         $this->call([
-            RoleSeeder::class,
             LoanSeeder::class,
-        ]);
-
-        $admin->update([
-            'role_id' => 1,
-        ]);
-
-        $staff->update([
-            'role_id' => 2,
         ]);
     }
 }
