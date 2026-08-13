c4ddd837f288b0280d273baa0a3f8cc0e077eccc
Omar Jabour <jabour.omar@hotmail.com>
2026-08-13 15:29:11 +0200

    :bento:TP4: MailLoan  maildev  + routes  :art:

--- bibliotheque-semaine18/resources/views/loans/index.blade.php
+++ bibliotheque-semaine18/resources/views/loans/index.blade.php
@@ -1,5 +1,11 @@
 <h1>Emprunts</h1>
 
+@if(session('success'))
+    <p>
+        {{ session('success') }}
+    </p>
+@endif
+
 <a href="{{ route('loans.create') }}">Nouvel emprunt</a>
 
 <table>
@@ -14,7 +20,9 @@
     @foreach($loans as $loan)
         <tr>
             <td>{{ $loan->user->name }}</td>
+
             <td>{{ $loan->book->title }}</td>
+
             <td>{{ $loan->borrowed_at }}</td>
 
             <td>
@@ -26,7 +34,9 @@
             </td>
 
             <td>
+
                 @if($loan->returned_at == null)
+
                     <form action="{{ route('loans.update', $loan) }}" method="POST">
                         @csrf
                         @method('PUT')
@@ -35,7 +45,17 @@
                             Marquer comme rendu
                         </button>
                     </form>
+
+                    <form action="{{ route('loans.remind', $loan) }}" method="POST">
+                        @csrf
+
+                        <button type="submit">
+                            Relancer
+                        </button>
+                    </form>
+
                 @endif
+
             </td>
         </tr>
     @endforeach
